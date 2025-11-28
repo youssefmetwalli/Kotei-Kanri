1ab40e030e4ce811993c4c4b07f8335cb31539a0
youssefmetwalli <ymetwalli22@gmail.com>
2025-11-28 09:20:57 +0900

    admin for system settings

--- pqms_backend/master/admin.py
+++ pqms_backend/master/admin.py
@@ -1,5 +1,17 @@
-
 from django.contrib import admin
-from .models import Category, CheckItem
+from .models import Category, CheckItem, SystemSettings
+
 admin.site.register(Category)
 admin.site.register(CheckItem)
+
+@admin.register(SystemSettings)
+class SystemSettingsAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "system_name",
+        "language",
+        "timezone",
+        "auto_backup",
+        "backup_frequency",
+        "updated_at",
+    )
