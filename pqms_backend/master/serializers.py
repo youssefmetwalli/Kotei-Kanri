44e06c8e91db31170cc77d657b5709cce3d40296
youssefmetwalli <ymetwalli22@gmail.com>
2025-11-28 09:12:35 +0900

    system settings serializer

--- pqms_backend/master/serializers.py
+++ pqms_backend/master/serializers.py
@@ -1,6 +1,6 @@
 
 from rest_framework import serializers
-from .models import Category, CheckItem
+from .models import Category, CheckItem, SystemSettings
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +15,29 @@ class CheckItemSerializer(serializers.ModelSerializer):
         fields = ["id","name","type","category","category_id","required","unit","description","options","created_at","updated_at",
                     "tags","min_value","max_value","decimal_places","default_value","error_message","allow_handwriting","reference_image",
                   ]
+
+
+class SystemSettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SystemSettings
+        fields = [
+            "id",
+            "system_name",
+            "language",
+            "timezone",
+            "date_format",
+            "user_name",
+            "email",
+            "role",
+            "email_notifications",
+            "task_notifications",
+            "report_notifications",
+            "system_alerts",
+            "two_factor_auth",
+            "session_timeout",
+            "password_expiry",
+            "auto_backup",
+            "backup_frequency",
+            "created_at",
+            "updated_at",
+        ]
